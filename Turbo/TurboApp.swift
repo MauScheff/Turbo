bd16b2d6119131e5d3d6477b7dc00c48de05ec90
Maurice Scheffmacher <maurice@scheffmacher.email>
2026-03-20 21:31:12 +0100

    Initial Commit

---
+++ Turbo/TurboApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurboApp.swift
+//  Turbo
+//
+//  Created by Maurice on 20.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TurboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TurboTests/TurboTests.swift
@@ -0,0 +1,17 @@
+//
+//  TurboTests.swift
+//  TurboTests
+//
+//  Created by Maurice on 20.03.2026.
+//
+
+import Testing
+@testable import Turbo
+
+struct TurboTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
